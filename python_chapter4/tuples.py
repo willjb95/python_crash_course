8b3c823a466c41f1ca8d0c14d0ba6b15b3ba2fc3
willjb95 <williambebbington0@hotmail.co.uk>
2021-04-26 19:20:11 +0100

    learning about tuples

---
+++ python_chapter4/tuples.py
@@ -0,0 +1,7 @@
+## TUPLES ##
+# lists work well for storing collections of items that can change throughout the life of a program. the aility to 
+# modify lists is when working with lists of users, sometimes you create a list of items that cannot change, tuples
+# allow you to do that. python refers to values liek that as immutable and an immutable list is called a tuple.
+
+## DEFINING A TUPLE ##
+# 
