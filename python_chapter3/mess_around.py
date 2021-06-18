20202c165189a5dbaca73da40307ac822a7a3b5c
willjb95 <williambebbington0@hotmail.co.uk>
2021-06-18 12:14:10 +0100

    Trying out new methods learned later on in the exercise

--- python_chapter3/mess_around.py
+++ python_chapter3/mess_around.py
@@ -4,3 +4,8 @@ a = "hello"
 names = ['william', 'joe', 'hannah', 'anna', 'niamh']
 for name in names:
     print(name)
+
+fav_food = ['pizza', 'pasta', 'steak', 'crisps', 'veg', 'noodles']
+print(fav_food[3].upper())
+print(fav_food[3].lower())
+print(f"My favorite food is: {fav_food[0].title()}")
