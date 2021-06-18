5c9b0f8a2b34b0c54fe90927c4017761f1d27606
willjb95 <williambebbington0@hotmail.co.uk>
2021-06-18 18:47:20 +0100

    used for testing and creating

--- python_chapter6/mess_around.py
+++ python_chapter6/mess_around.py
@@ -5,3 +5,8 @@ alien_1['health'] = 100
 # adding new key-value pair to dictionary 
 print(alien_1)
 # printing to make sure it works correctly 
+enemy_1 = {}
+enemy_1['damage'] = 25
+# created enemy dictionary, added key-value
+# pair stating its damage
+  
